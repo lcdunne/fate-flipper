f8c68e2c6e110697363994491a0100ed8f7ce5f8
lcdunne <56237933+lcdunne@users.noreply.github.com>
2023-12-30 16:27:02 +0000

    Structure into functions

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ class Colours:
     text = "#3B4252"
     window_bg = "#ECEFF4"
     alt_bg = "#D8DEE9"
+    danger = "#BF616A"
+    success = "#8FBCBB"
 
 
 class App:
@@ -18,13 +20,27 @@ class App:
         self.root.geometry("400x200")
         self.root.title("Fate Flipper")
         self.root.iconbitmap("assets/vecteezy_whale-tail-symbol_6720669-1_resized.ico")
-
-        # Widgets inside frame
         self.mainframe = tk.Frame(self.root, background=Colours.window_bg)
         self.mainframe.pack(fill="both", expand=True)
-
+        # Center all content
+        self.mainframe.columnconfigure(0, weight=1)  # All available H space
+        self.mainframe.rowconfigure(0, weight=1)  # All available V space
+        self.mainframe.rowconfigure(1, weight=1)
+        # Setup for main app content
         self.bias = tk.IntVar(value=50)
+        self._setup_label()
+        self._setup_randomization_btn()
 
+        style = ttk.Style()
+        style.configure(
+            "TButton", font=("Arial", 12), padding=(10, 5), foreground=Colours.text
+        )
+        self._setup_slider()
+
+        self.root.mainloop()
+        return
+
+    def _setup_label(self):
         self.text = ttk.Label(
             self.mainframe,
             text="",
@@ -33,28 +49,18 @@ class App:
             font=("Arial", 26),
         )
         self.text.grid(row=0, column=0, columnspan=2, pady=10)
-        self.flip()
+        self.set_flip_value()
 
+    def _setup_randomization_btn(self):
         self.btn_flip = ttk.Button(
             self.mainframe,
-            text="go",
+            text="Go",
             command=self.flip,
         )
         self.btn_flip.grid(row=1, column=0, pady=10, sticky="ns")
 
-        # Center all content
-        self.mainframe.columnconfigure(0, weight=1)  # All available H space
-        self.mainframe.rowconfigure(0, weight=1)  # All available V space
-
-        # Increase the vertical height of the button
-        self.mainframe.rowconfigure(1, weight=1)
-
-        # Style the button
-        style = ttk.Style()
-        style.configure(
-            "TButton", font=("Arial", 12), padding=(10, 5), foreground=Colours.text
-        )
-
+    def _setup_slider(self):
+        # Slider to set the bias
         self.bias_slider = ttk.Scale(
             self.mainframe,
             orient="horizontal",
@@ -63,7 +69,6 @@ class App:
             variable=self.bias,
             command=lambda _: self.bias_label.config(text=f"{self.bias.get()}%"),
         )
-        self.bias_slider.grid(row=2, column=0, pady=10, sticky="nsew")
 
         # Small label for the slider
         self.bias_label = ttk.Label(
@@ -72,17 +77,21 @@ class App:
             foreground=Colours.text,
             font=("Arial", 12),
         )
+
+        self.bias_slider.grid(row=2, column=0, pady=10, sticky="nsew")
         self.bias_label.grid(row=3, column=0)
 
-        self.root.mainloop()
-        return
-
-    def flip(self):
-        y = random.random()
+    def set_flip_value(self, y=None):
+        y = y or self.bias.get() / 100
         self.text.config(
             text=f"{y:.0%}",
-            background="#BF616A" if y >= self.bias.get() / 100 else "#8FBCBB",
+            background=Colours.danger
+            if y >= self.bias.get() / 100
+            else Colours.success,
         )
+
+    def flip(self):
+        self.set_flip_value(random.random())
 
 
 if __name__ == "__main__":
